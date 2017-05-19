a043411caeb9d2d77368966d66b414ea734f708d
xiananliu <xiananliu@lexinfintech.com>
2017-05-19 17:59:02 +0800

    增加game、增加404页面

--- dist/game/index.js
+++ dist/game/index.js
@@ -1,2 +1,2 @@
-/*! The file is creted by lxa --Fri May 19 2017 17:06:48 GMT+0800 (中国标准时间) */
+/*! The file is creted by lxa --Fri May 19 2017 17:58:40 GMT+0800 (中国标准时间) */
 webpackJsonp([4],{2:function(o,n){console.log("this is page 1"),console.log($(window).width())}},[2]);
--- dist/tool/jinzhi/index.js
+++ dist/tool/jinzhi/index.js
@@ -1,2 +1,2 @@
-/*! The file is creted by lxa --Fri May 19 2017 17:06:48 GMT+0800 (中国标准时间) */
+/*! The file is creted by lxa --Fri May 19 2017 17:58:40 GMT+0800 (中国标准时间) */
 webpackJsonp([1],{5:function(t,r,n){function a(t,r,n){if(""==t||!r||!n)return"";if(r<2||r>36)return alert("输入进制只能介于2-36之间！"),NaN;if(n<2||n>36)return alert("输出进制只能介于2-36之间！"),NaN;try{return parseInt(t,r).toString(n)}catch(t){return NaN}}function u(){var t=a($("#number").val(),parseInt($("#input_radix").val()),parseInt($("#output_radix").val()));$("#result").html(t)}n(0),$("#input_radix,#output_radix,#number").on("input",u),u()}},[5]);
