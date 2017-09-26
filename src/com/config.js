2fff279387a4953804720b06540250ba22d16c32
liuxianan <937925941@qq.com>
2017-09-26 21:59:03 +0800

    修改域名

--- src/com/config.js
+++ src/com/config.js
@@ -2,8 +2,8 @@
 var config = 
 {
 	protocol: 'http', // 协议，为将来升级https预留
-	// domain: 'liuxianan.com', // 主站域名
-	domain: 'xiaoming.com', // 主站域名
+	domain: 'liuxianan.com', // 主站域名
+	// domain: 'xiaoming.com', // 主站域名
 	siteName: '柳夏南的小窝' // 主站名称
 };
 // 资源站点路径
